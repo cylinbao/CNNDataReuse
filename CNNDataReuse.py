554da9ed64dc97dbd95e8e852f879dc068e6bd2c
Chien Yu Lin <myislin@gmail.com>
2016-09-26 10:03:36 +0800

    can get right answer

--- CNNDataReuse.py
+++ CNNDataReuse.py
@@ -2,20 +2,19 @@ import math
 
 def calIReuse(I_size, F_size):
     I_reuse = 0;
-    for y in range(1, I_size):
-        y_dis = min(y - 1, I_size - y)
-        if (y_dis >= (F_size - 1)) 
+    for y in range(0, I_size):
+        y_dis = min(y - 0, I_size -1 - y)
+        if y_dis >= (F_size - 1):
             y_reuse = F_size
-        else
+        else:
             y_reuse = y_dis + 1
-        for x in range(1, I_size):
-            x_dis = min(x - 1, I_size - x)
-            if (x_dis >= (F_size - 1))
+        for x in range(0, I_size):
+            x_dis = min(x - 0, I_size -1 - x)
+            if x_dis >= (F_size - 1):
                 x_reuse = F_size
-            else
+            else:
                 x_reuse = x_dis + 1
             I_reuse += x_reuse*y_reuse
-
     return(I_reuse)
 
 F_size = int(input("Enter Filter size:"))
@@ -23,8 +22,10 @@ F_num = int(input("Enter Filter number:"))
 I_size = int(input("Enter Image size:"))
 
 F_reuse = pow((I_size - F_size + 1), 2)
-#I_reuse = pow(F_size, 2)*F_num
-I_reuse = calIReuse(I_size, F_size)
+I_reuse_total = calIReuse(I_size, F_size)
+I_reuse = I_reuse_total * F_num / pow(I_size, 2)
 
-print("Average Reuse of Filter is: ", F_reuse)
+print()
+print("Average Reuse of Filter is:", F_reuse)
+#print("Total Reuse of Image is:", I_reuse_total)
 print("Average Reuse of Image is: ", I_reuse)
