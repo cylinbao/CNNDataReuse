de369269cd7ff632994f31cb07eeb291d9da6d0a
Chien Yu Lin <myislin@gmail.com>
2016-09-26 15:39:30 +0800

    consider stride

--- CNNDataReuse.py
+++ CNNDataReuse.py
@@ -1,14 +1,14 @@
-import math
+from math import *
 
-def calIReuse(I_size, F_size):
+def calIReuse(I_size, F_size, stride):
     I_reuse = 0;
-    for y in range(0, I_size):
+    for y in range(0, I_size, stride):
         y_dis = min(y - 0, I_size -1 - y)
         if y_dis >= (F_size - 1):
             y_reuse = F_size
         else:
             y_reuse = y_dis + 1
-        for x in range(0, I_size):
+        for x in range(0, I_size, stride):
             x_dis = min(x - 0, I_size -1 - x)
             if x_dis >= (F_size - 1):
                 x_reuse = F_size
@@ -19,13 +19,16 @@ def calIReuse(I_size, F_size):
 
 F_size = int(input("Enter Filter size:"))
 F_num = int(input("Enter Filter number:"))
+stride = int(input("Enter the Stride:"))
 I_size = int(input("Enter Image size:"))
 
-F_reuse = pow((I_size - F_size + 1), 2)
-I_reuse_total = calIReuse(I_size, F_size)
+#output_size = ceil((I_size - F_size)/stride) + 1
+F_reuse = pow(output_size, 2)
+#I_reuse_total = calIReuse(I_size, F_size, stride)
 I_reuse = I_reuse_total * F_num / pow(I_size, 2)
 
 print()
+print("Output Size is:", output_size)
 print("Average Reuse of Filter is:", F_reuse)
-#print("Total Reuse of Image is:", I_reuse_total)
+print("Total Reuse of Image is:", I_reuse_total)
 print("Average Reuse of Image is: ", I_reuse)
